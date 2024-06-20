5a4c4c27f4ecc13a6e095181a75f022c60c1042d
Kartik Sardhara <kartiksardhara25@gmail.com>
2024-06-20 14:52:12 +0530

    nothing

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { DeleteUser } from "./userReducer";
 function Home() {
   const user = useSelector((state) => state.user);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handkeDelete = (id) => {
